0c80acdf62149be0d5c4ae06987c4872a0861314
Sarker Siam <35716510+siam4201@users.noreply.github.com>
2026-06-20 21:11:02 +0600

    case_officer dto and routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CaseOfficerModule } from './case_officer/case_officer.module';
 
 @Module({
-  imports: [],
+  imports: [CaseOfficerModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/case_officer/dto/create-case.dto.ts
@@ -0,0 +1,7 @@
+export class CreateCaseDto {
+  name: string;
+  age: number;
+  lastSeenLocation: string;
+  description: string;
+  contactNumber: string;
+}
