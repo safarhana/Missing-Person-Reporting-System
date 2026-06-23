5974b0c9d47dfaa2594a211d57e0ddf3888ff99f
WASI-U-ISLAM <islamwasi891@gmail.com>
2026-06-23 22:02:07 +0600

    Missing Person Reporter Added!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MprModule } from './missing_person_reporter/mpr.module';
 
 @Module({
-  imports: [],
+  imports: [MprModule],
   controllers: [AppController],
   providers: [AppService],
 })
