068a2f803a8c13eac6e03a9df60de89cc52e2514
turag121 <tahmidturag95@gmail.com>
2026-07-15 20:29:26 +0600

    volunteer-typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
-
 import { VolunteerModule } from './volunteer/volunteer.module';
 import { CaseOfficerModule } from './case_officer/case_officer.module';
-import { AdminModule } from './admin/admin.module';
 import { MprModule } from './missing_person_reporter/mpr.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234', 
+      database: 'volunteer_db', 
+
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     VolunteerModule,
     CaseOfficerModule,
     AdminModule,
