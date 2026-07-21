748d7cb768e87e9dba7028083fe047245e2008f3
safarhana <farhana.mim080@gmail.com>
2026-07-21 22:31:49 +0600

    Implement Admin relationships with Volunteer and Case Officer

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -5,12 +5,21 @@ import { Admin } from './admin.entity';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 
+
+import { VolunteerEntity } from '../volunteer/volunteer.entity';
+import { CaseOfficerEntity } from '../case_officer/case_officer.entity';
+
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Admin])
+   
+    TypeOrmModule.forFeature([
+      Admin,
+      VolunteerEntity,
+      CaseOfficerEntity,
+    ]),
   ],
 
   controllers: [AdminController],
-  providers: [AdminService]
+  providers: [AdminService],
 })
 export class AdminModule {}
