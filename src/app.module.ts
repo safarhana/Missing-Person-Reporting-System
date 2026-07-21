e39cc1f78c84617441c2f378372c27ae2962b9b1
safarhana <farhana.mim080@gmail.com>
2026-07-22 01:45:48 +0600

    Configure mailer using environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AdminModule } from './admin/admin.module';
 import { VolunteerModule } from './volunteer/volunteer.module';
 import { CaseOfficerModule } from './case_officer/case_officer.module';
 import { MprModule } from './missing_person_reporter/mpr.module';
-
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -28,11 +34,9 @@ import { AuthModule } from './auth/auth.module';
     AdminModule,
     MprModule,
     AuthModule,
-    
   ],
 
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
