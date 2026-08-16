177970256ce07c572057ff99cd3f493479296327
turag121 <tahmidturag95@gmail.com>
2026-08-17 01:25:21 +0600

    Update volunteer database and  mailer (third)

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,15 +19,22 @@ import { MailerModule } from '@nestjs-modules/mailer';
       envFilePath: '.env',
     }),
 
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'mysql',
-      database: 'missing_person_reporting_system',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
 
    
